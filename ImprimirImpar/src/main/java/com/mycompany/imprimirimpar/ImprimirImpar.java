fd13999dbeffe344fb227155c42350af5cf085e2
AparicioFermiano <aparicio.empresarial@gmail.com>
2022-09-13 21:23:26 -0300

    Atividade de impar e par para as aulas do dia 02 e dia 09 de setembro

---
+++ ImprimirImpar/src/main/java/com/mycompany/imprimirimpar/ImprimirImpar.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.imprimirimpar;
+
+/**
+ *
+ * @author aparicio
+ */
+public class ImprimirImpar {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            if (i % 2 != 0){
+                System.out.println(i);
+            }  
+        }
+    }
+}
---
+++ ImprimirPares/src/main/java/com/mycompany/imprimirpares/ImprimirPares.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.imprimirpares;
+
+/**
+ *
+ * @author aparicio
+ */
+public class ImprimirPares {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 50){
+            if(i % 2 == 0){
+                System.out.println(i);
+                i++;
+            } else{
+                i++;
+            }   
+        }
+    }
+}
